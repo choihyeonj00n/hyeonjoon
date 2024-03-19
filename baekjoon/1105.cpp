9ab134ad28cc18f74cab9d9d5c9a21c6553f5d9a
hyeonjoonChoi <b911902@soongsil.ac.kr>
2024-03-19 06:16:07 +0000

    added

--- baekjoon/1105.cpp
+++ baekjoon/1105.cpp
@@ -3,32 +3,54 @@
 #define MAX_NUM 9
 using namespace std;
 
-int how_many_eight(int num){
-		int array[10];
-		for(int i = 0; i < 10; i++){
+int how_many_eight(int L, int R, int LR_long, int long_diff){
+		int array[2][long_diff];
+		for(int i = 0; i < long_diff; i++){
 				array[i] = 0;
 		}
+
+		for(int i = 0; i < 2; i++){
+				for(int j = LR_long; j >= long_diff; j++){
+						int a = j - long_diff;
+						array[i][a];
+				}
+		}
+
+		return 0;
+}
+
+int what_num(int num, int diff){
+
+}
+
+int how_long(int num){
 		int n = 0;
 		while(num / (int)pow(10, n) != 0){
-				int num1 = num / (int)pow(10, n);
-				int num2 = num1 % 10;
-				array[num2] += 1;
 				n++;
 		}
-		return array[8];
+		return n;
 }
 
 int main(){
 		int L, R;
 		int min = MAX_NUM;
 		cin >> L >> R;
-		for(int i = L; i <= R; i++){
-				int comp = how_many_eight(i);
-				if(min > comp){
-						min = comp;
+		int diff = R - L;
+		int L_long = how_long(L);
+		int R_long = how_long(R);
+		int diff_long = how_long(diff);
+		if(L_long != R_long){
+				cout << 0 << endl;
+		}
+		else{
+				if(L_long == diff_long){
+						cout << 0 << endl;
+				}
+				else{
+						int long_diff = L_long - diff_long;
+						
 				}
 		}
 
-		cout << min << endl;
 		return 0;
 }
--- baekjoon/test.cpp
+++ baekjoon/test.cpp
@@ -5,23 +5,6 @@
 using namespace std;
 
 int main(){
-		vector<int> v = {1,2,3,4,5};
-		swap(v[1], v[4]);
-		for(int i = 0; i < 5; i++){
-				cout << v[i] << " ";
-		}
-		cout << endl;
-		string str1 = "aello";
-		string str2 = "no";
-		cout << str1 << " : " << str1.length() << endl;
-		cout << str2 << " : " << str2.length() << endl;
-		if (str1 == str2){
-				cout << "same" << endl;
-		}
-		else if(str1 < str2){
-				cout << "right" << endl;
-		}
-
-
+		cout << 0 << endl;
 		return 0;
 }
