7eb386f2e3b96dac86372ff5578027648e05c4b8
hyeonjoonChoi <b911902@soongsil.ac.kr>
2024-03-21 02:21:43 +0000

    added

--- baekjoon/1105.cpp
+++ baekjoon/1105.cpp
@@ -12,15 +12,12 @@ int how_many_eight(int L, int R, int LR_long, int long_diff, int diff_long){
 				array[1][i] = 0;
 		}
 
-		cout << "------------is it right?--------------" << endl;
 		for(int j = LR_long; j > diff_long; j--){
 				int a = j - diff_long - 1;
 				int num1 = L / (int)pow(10, j-1);
 				int num2 = num1 % 10;
-				cout << num2 << endl;
 				int num3 = R / (int)pow(10, j-1);
 				int num4 = num3 % 10;
-				cout << num4 << endl;
 				array[0][a] = num2;
 				array[1][a] = num4;
 		}
@@ -32,11 +29,6 @@ int how_many_eight(int L, int R, int LR_long, int long_diff, int diff_long){
 				else if(array[0][i] == array[1][i] && array[0][i] == 8){
 						count += 1;
 				}
-		}
-
-		for(int i = 0; i < size; i++){
-				cout << "L's" << i << " th number : " << array[0][i] << endl;
-				cout << "R's" << i << " th number : " << array[1][i] << endl;
 		}
 
 		return count;
@@ -58,7 +50,6 @@ int main(){
 		int L_long = how_long(L);
 		int R_long = how_long(R);
 		int diff_long = how_long(diff);
-		cout << "L_long : " << L_long << " R_long : " << R_long << " diff_long : " << diff_long << endl;
 		if(L_long != R_long){
 				cout << 0 << endl;
 		}
