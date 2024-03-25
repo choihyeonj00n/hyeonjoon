f8094ead007cae432bf965cd21ccb13cbc7d63de
hyeonjoonChoi <b911902@soongsil.ac.kr>
2024-03-25 03:14:01 +0000

    add

--- baekjoon/Class/1068.cpp
+++ baekjoon/Class/1068.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <utility>
 using namespace std;
 
-class Node
-{
-private :		
-
-		int SelfNode;
-		int ParentNode;
-		int *SonNode;
-
-public :
-
-		Node(int _SelfNode, int _ParentNode, int* _SonNode);
-		int FindParent( 
-};
+void DFS(int node, vector<int> visited, vector<pair<int, vector<int>>> graph){
+}
 
 int main()
 {
 		//setting initial condition
 		int NumberOfNode;
 		cin >> NumberOfNode;
-		int graph[NumberOfNode];
+		int Node[NumberOfNode];
 		for(int i = 0; i < NumberOfNode; i++){
-				cin >> graph[i];
+				cin >> Node[i];
 		}
 		int RemoveNode;
 		cin >> RemoveNode;
+		vector<int> visited(NumberOfNode, 0); //To check its node visited 0 : nonvisited -1 : visited
+		vector<pair<int, vector<int>>> graph; //graph : vector<parent_node, child>
+		for(int i = 0; i < NumberOFNode; i++){
+				vector<int> node; //vector such that include each node's child
+				for(int j = 0; j < NumberOfNode; j++){
+						if(i == Node[j]){
+								node.push_back(j);
+						}
+				}
+				if(node.empty()){
+						node.push_back(-1);
+				}
+				graph.push_back(make_pair(Node[i], node));
+		}
+
+		//find root node
+		int root;
+		for(int i = 0; i < NumberOfNode; i++){
+				if(graph[i].first == -1){
+						root = i;
+						break;
+				}
+		}
+
+		//first remove RemoveNode
+		visited[RemoveNode] = -1;
+		int size = graph[RemoveNode].second.size();
+		
+
 
 		return 0;
 }
--- baekjoon/implementation/test.cpp
+++ baekjoon/implementation/test.cpp
@@ -2,10 +2,14 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <utility>
 using namespace std;
 
 int main(){
-		char a = 'C';
-		cout << a << endl;
+		vector<pair<int, vector<int>>> a;
+		int i = 1;
+		vector<int> b = {1,2,3};
+		a.push_back(make_pair(i, b));
+		cout << a[0].second[1] << endl;
 		return 0;
 }
