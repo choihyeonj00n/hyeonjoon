59c00c689deaea661459c69026fcf9d9c4a36e90
choihyeonj00n <163098655+choihyeonj00n@users.noreply.github.com>
2024-03-29 10:54:42 +0900

    1080.cpp 업데이트

--- baekjoon/implementation/1080.cpp
+++ baekjoon/implementation/1080.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main()
 {
+		//setting initial condition
 		int row, col;
 		cin >> row >> col;
+		int input[row][col];
+		int output[row][col];
+		for(int i = 0; i < row; i++){
+				for(int j = 0; j < col; j++){
+						int num;
+						cin >> num;
+						if(num != 0 && num != 1){
+								cout << "0 과 1만 행렬에 넣을 수 있습니다." << endl;
+						}
+						input[i]][j] = num;
+				}
+		}
+		for(int i = 0; i < row; i++){
+				for(int j = 0; j < col; j++){
+						int num;
+						cin >> num;
+						if(num != 0 && num != 1){
+								cout << "0 과 1만 행렬에 넣을 수 있습니다." << endl;
+						}
+						output[i]][j] = num;
+				}
+		}
 		return 0;
 }
